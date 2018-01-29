a19546aa13371bd308c8dd958bc667cdf7c2f79d
Sergi Valverde <sergi.valverde@udg.edu>
2018-01-29 18:10:18 +0100

    pull from dockerhub

--- nic_train_network_batch_docker.sh
+++ nic_train_network_batch_docker.sh
@@ -6,20 +6,11 @@
 # Sergi Valverde 2018
 # --------------------------------------------------
 
-# check if the docker is running
-DOCKER_NAME='nicmslesions'
-DOCKER_RUNNING=`docker images | grep nicmslesions | wc -w`
 
-if [ $DOCKER_RUNNING -gt 0 ];
-then
-    nvidia-docker run -ti  \
-                  -v config:/home/user/config \
-                  -v /:/data:rw \
-                  nicmslesions python -u nic_train_network_batch.py --docker | tee log.txt
-else
-    docker build -f Dockerfile -t nicmslesions . &&
-    nvidia-docker run -ti  \
-                  -v config:/home/user/config \
-                  -v /:/data:rw \
-                  nicmslesions python -u nic_train_network_batch.py --docker | tee log.txt
-fi
+# check new versions
+docker pull nicvicorob/mslesions:latest
+
+nvidia-docker run -ti  \
+              -v config:/home/user/config \
+              -v /:/data:rw \
+              nicvicorob/mslesions:latest python -u nic_train_network_batch.py --docker | tee log.txt
